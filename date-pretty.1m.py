4d1c60a325df0e8299ff02948b6730a883eaaa49
raochsinai <roachsinai@qq.com>
2020-01-01 00:23:38 +0800

    fix error that use year of solar date to calculate whether it is a leap year.

--- date-pretty.1m.py
+++ date-pretty.1m.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# install LunarCalendar: https://github.com/wolfhong/LunarCalendar
-# if you install LunarCalendar from pypi, you should use
-# LunarCalendar.converter replace lunarcalendar.converter below
 
 import datetime
 import calendar
@@ -12,18 +9,19 @@ from itertools import cycle
 
 
 today = datetime.datetime.today()
+# today = datetime.datetime(2019, 3, 31)
 today_date = today.date()
 year = today.year
 month = today.month
 
 def pstatus_bar():
     '''print status bar'''
-    bitbar="|font=mononoki size=14 color=black dropdown=false"
+    bitbar="|font=iosevka size=14 color=white dropdown=false"
     birthdays = {"0102": "Som"}
     gap_numbers = {0: "!", 1: "'", 2: "╎", 3: "┆", 4: "┊"}
     is_important= False
-    is_leap = (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
     lunar_year = Lunar.from_date(today_date).year
+    is_leap = (lunar_year % 4 == 0 and (lunar_year % 100 != 0 or lunar_year % 400 == 0))
     days_this_year = 366 if is_leap else 365
     for lunar_date in birthdays.keys():
         lunar_month, lunar_day = int(lunar_date[-4:-2]), int(lunar_date[-2:])
@@ -48,7 +46,7 @@ def pstatus_bar():
                     is_important = True
 
     if not is_important:
-        print(f"{list(calendar.day_abbr)[today.weekday()]}{today.day}⸾{today.hour:02d}:{today.minute:02d}{bitbar}")
+        print(f"{list(calendar.day_abbr)[today.weekday()]}{today.day:02d} {today.hour:02d}:{today.minute:02d}{bitbar}")
 
 
 def pdropdown():
@@ -70,19 +68,24 @@ def pdropdown():
            "廿六","廿七","廿八","廿九","三十"
           )
     c_mon = ("","正","二","三","四","五","六","七","八","九","十","十一","腊")
+    count_days_to_show = 0
     for weekday, day, date in zip(cycle(cal.iterweekdays()), cal.itermonthdays(year, month), cal.itermonthdates(year, month)):
+        count_days_to_show += 1
         if date in solar2fest:
             lunar_calendar_info = solar2fest[date][0][:2]
-            s += f"{day:02d}{lunar_calendar_info} "
+            if day == today.day:
+                s += f"<font color=\"yellow\">{day:02d}{lunar_calendar_info}</font> "
+            else:
+                s += f"{day:02d}{lunar_calendar_info} "
         else:
             solar = Solar.from_date(date)
             lunar = Converter.Solar2Lunar(solar)
             if lunar.day == 1:
-                lunar_calendar_info = c_mon[lunar.month]
+                lunar_calendar_info = c_mon[lunar.month] + "月"
             else:
                 lunar_calendar_info = c_day[lunar.day]
             if day == today.day:
-                s += f"<font color=\"blue\">{day:02d}{lunar_calendar_info}</font> "
+                s += f"<font color=\"yellow\">{day:02d}{lunar_calendar_info}</font> "
             elif day == 0:
                 s += f"<font color=\"gray\">{date.day:02d}{lunar_calendar_info}</font> "
             else:
@@ -95,12 +98,13 @@ def pdropdown():
            s = ""
 
     to_show = [pstring.rstrip() for pstring in to_show]
-    bitbar = "| font='FiraMono' size=6 color=black"
-    print(bitbar)
-    bitbar = "| font='FiraMono' size=14 color=black"
+    bitbar = "| font='FiraMono' size=6 color=white"
+    if count_days_to_show != 42:
+        print(bitbar)
+    bitbar = "| font='FiraMono' size=14 color=white"
     print("\\n".join(to_show) + bitbar)
     print(bitbar)
-    bitbar = "| font='FiraMono' size=16 color=black"
+    bitbar = "| font='FiraMono' size=16 color=white"
 
     lunar = Lunar.from_date(today_date)
     lunar_year = lunar.year
